0e2e97e30c7b6c8cc61a08b122d1f34de917bfca
Adrienne Kinney <akinney1@arizona.edu>
2025-10-29 03:22:33 -0400

    Adjust legend for forecast plots

--- utils/plotting_utils.py
+++ utils/plotting_utils.py
@@ -15,50 +15,42 @@ import utils.forecasting_utils as forecast_utils
 import utils.analysis_utils as analysis_utils
 
 import matplotlib.dates as mdates
-from matplotlib.lines import Line2D
-from matplotlib.patches import Rectangle
-from matplotlib.legend_handler import HandlerTuple
-
 from sklearn.metrics import mean_squared_error as mse
 
 # autopep8: on
 
 
 #---------------Representative Forecasts functions
-class HandlerLineOverWideBand(HandlerTuple):
-    """Custom legend entry showing a wide band with a line centered in it."""
-    def create_artists(self, legend, orig_handle,
-                       xdescent, ydescent, width, height, fontsize, trans):
-        line, band = orig_handle  # tuple (line_handle, band_handle)
+def create_legend(fig, bottom_margin, bbox_to_anchor):
+    from matplotlib.lines import Line2D
+    from matplotlib.patches import Rectangle
+    from matplotlib.legend_handler import HandlerTuple
 
-        # Make the band fill ~80% of the vertical space
-        band_height = 1.5 * height
-        band_y = ydescent - 0.25 * height  # center the band vertically
-        band_artist = Rectangle(
-            (xdescent, band_y), width, band_height,
-            facecolor=band.get_facecolor(),
-            alpha=band.get_alpha(),
-            edgecolor='none',
-            transform=trans
-        )
+    class HandlerLineOverWideBand(HandlerTuple):
+        def create_artists(self, legend, orig_handle,
+                           xdescent, ydescent, width, height, fontsize, trans):
+            line, band = orig_handle
+            band_height = 1.5 * height
+            band_y = ydescent + (height - band_height) / 2
+            band_artist = Rectangle(
+                (xdescent, band_y), width, band_height,
+                facecolor=band.get_facecolor(), alpha=band.get_alpha(),
+                edgecolor='none', transform=trans,
+            )
+            line_y = band_y + band_height / 2
+            line_artist = Line2D(
+                [xdescent, xdescent + width], [line_y, line_y],
+                color=line.get_color(), linestyle=line.get_linestyle(),
+                linewidth=line.get_linewidth(), transform=trans,
+            )
+            return [band_artist, line_artist]
 
-        # Draw the line exactly through the middle of the band
-        line_y = band_y + band_height / 2
-        line_artist = Line2D(
-            [xdescent, xdescent + width],
-            [line_y, line_y],
-            color=line.get_color(),
-            linestyle=line.get_linestyle(),
-            linewidth=line.get_linewidth(),
-            transform=trans
-        )
+    # Reserve bottom margin for the legend
+    fig.subplots_adjust(bottom=bottom_margin)
 
-        return [band_artist, line_artist]
-
-def create_legend(fig):
     trap_handle = Line2D([], [], marker='o', linestyle='None',
-                        color='tab:orange', markerfacecolor='tab:orange',
-                        markeredgewidth=0, markersize=8)
+                         color='tab:orange', markerfacecolor='tab:orange',
+                         markeredgewidth=0, markersize=8)
 
     line_handle = Line2D([], [], color='tab:blue', linewidth=2)
     band_handle = Rectangle((0, 0), 1, 1, facecolor='tab:blue', alpha=0.35, edgecolor='none')
@@ -69,15 +61,20 @@ def create_legend(fig):
     handles = [trap_handle, prob_handle, true_handle]
     labels  = ['Trap samples', 'Probabilistic Forecast', 'True prediction\ninterval bounds']
 
-    fig.legend(
+    leg = fig.legend(
         handles, labels,
         handler_map={tuple: HandlerLineOverWideBand()},
-        loc='lower center', bbox_to_anchor=(0.5, -0.1),
+        loc='lower center', bbox_to_anchor=bbox_to_anchor,
         ncol=3, fontsize='medium',
         frameon=True, fancybox=True, edgecolor='0.7',
         handlelength=2.2, columnspacing=2.0, labelspacing=1.0
     )
+
+    # Keep legend out of layout calculations so it won't be clipped
+    leg.set_in_layout(False)
+    
     return fig
+
 
 def format_single_plot(ax, sample, dist):
     trial_cols = forecast_utils.trial_names()
